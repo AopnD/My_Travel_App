57acfa0545dbb5f791007043a703ddd97a0898e9
omer <aopnd@outlook.com>
2022-03-21 20:52:39 +0200

    finnished card component and fixing fetch problem

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -7,9 +7,63 @@ import Rating from '@material-ui/lab/Rating';
 import useStyles from './styles'
 
 const PlaceDetails = ({ place }) =>{
+    const classes = useStyles();
+
     return (
-        <h1>{place.name}</h1>
+        <Card elevation={6}>
+            <CardMedia 
+            style={{height: 350}}
+            image={place.photo ? place.photo.images.large.url : 'https://1080motion.com/wp-content/uploads/2018/06/NoImageFound.jpg.png'}
+            title={place.name}
+            />
+            <CardContent>
+                <Typography gutterBottom variant='h5'>{place.name}</Typography>
+                <Box display="flex" justifyContent="space-between">
+                    <Typography variant='subtitel1'>Price</Typography>
+                    <Typography gutterBottom variant='subtitel1'>{place.price_level}</Typography>
+                </Box>
+                <Box display="flex" justifyContent="space-between">
+                    <Typography variant='subtitel1'>Ranking</Typography>
+                    <Typography gutterBottom variant='subtitel1'>{place.ranking}</Typography>
+                </Box>
+
+{place?.awards?.map((award)=> (
+<Box  my={1} display="flex" justifyContent="space-btween">
+<img src={award.images.small} alt={award.display_name}/>
+<Typography variant='subtitle2' color="textSecondary">{award.display_name}</Typography>
+</Box>
+))}
+
+{place?.cuisine?.map(({name})=>(
+<Chip key={name} size="small" label={name} className={classes.chip}/>
+))}
+
+{place?.address && (
+    <Typography gutterBottom variant='subtitle2' color="textSecondary" className={classes.subtitle}>
+        <LocationOnIcon/> {place.address}
+    </Typography>
+)}
+
+{place?.phone && (
+    <Typography gutterBottom variant='subtitle2' color="textSecondary" className={classes.spacing}>
+        <PhoneIcon/> {place.phone}
+    </Typography>
+)}
+
+<CardActions>
+    <Button size="small" color='primary' onClick={()=> window.open(place.web_url, '_blank')}>
+        Trip Advisor
+    </Button>
+    <Button size="small" color='primary' onClick={()=> window.open(place.website, '_blank')}>
+        Website
+    </Button>
+</CardActions>
+
+            </CardContent>
+            
+        </Card>
     );
 }
+
 
 export default PlaceDetails;
